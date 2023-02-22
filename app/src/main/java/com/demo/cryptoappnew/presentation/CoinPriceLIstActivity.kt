d71ac5f9a567110b60d1af43c6675e1cd3fab4e6
nebulove <nebuloff@googlemail.com>
2023-02-22 16:37:59 +0400

    add scrollview to fragment

--- app/src/main/java/com/demo/cryptoappnew/presentation/CoinPriceLIstActivity.kt
+++ app/src/main/java/com/demo/cryptoappnew/presentation/CoinPriceLIstActivity.kt
@@ -3,6 +3,7 @@ package com.demo.cryptoappnew.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.demo.cryptoappnew.R
 import com.demo.cryptoappnew.databinding.ActivityCoinPriceListBinding
 import com.demo.cryptoappnew.domain.CoinInfo
 import com.demo.cryptoappnew.presentation.apapters.CoinInfoAdapter
@@ -21,19 +22,41 @@ class CoinPriceLIstActivity : AppCompatActivity() {
         val adapter = CoinInfoAdapter(this)
         adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
             override fun onCoinClick(coinPriceInfo: CoinInfo) {
-                val intent = CoinDetailActivity.newIntent(
-                    this@CoinPriceLIstActivity,
-                    coinPriceInfo.fromSymbol
-                )
-                startActivity(intent)
+                if (isOnePaneMode()) {
+                    launchDetailActivity(coinPriceInfo.fromSymbol)
+                } else {
+                    launchDetailFragment(coinPriceInfo.fromSymbol)
+                }
             }
         }
+
         binding.rvCoinPriceList.adapter = adapter
         binding.rvCoinPriceList.itemAnimator = null
-        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
-        viewModel.coinInfoList.invoke().observe(this){
+        viewModel = ViewModelProvider(this)[CoinViewModel::
+        class.java]
+        viewModel.coinInfoList.invoke().observe(this)
+        {
             adapter.submitList(it)
         }
+    }
+
+    private fun isOnePaneMode() = binding.fragmentContainer == null
+
+    private fun launchDetailActivity(fromSymbol: String) {
+        val intent = CoinDetailActivity.newIntent(
+            this@CoinPriceLIstActivity,
+            fromSymbol
+        )
+        startActivity(intent)
+    }
+
+    private fun launchDetailFragment(fromSymbol: String) {
+        supportFragmentManager.popBackStack()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, CoinDetailFragment.newInstance(fromSymbol))
+            .addToBackStack(null)
+            .commit()
     }
 }
 
